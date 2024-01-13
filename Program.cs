7b4fdf2b2b82aca9b81342812d9bbaba8a294524
A252460 <A252460@LSBSA-416QCS3>
2024-01-13 09:26:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+
+            bool isPalindrome = true;
+            for (int i = 0; i < word.Length / 2; i++)
+            {
+                if (char.ToLower(word[i]) != char.ToLower(word[word.Length - i - 1]))
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if (isPalindrome)
+            {
+                Console.WriteLine("The word {0} is a palindrome.", word);
+            }
+            else
+            {
+                Console.WriteLine("The word {0} is not a palindrome.", word);
+            }
+            Console.ReadLine();
+        }
+    }
+}
